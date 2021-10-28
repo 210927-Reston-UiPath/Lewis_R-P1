6e582d2ce7b0340b17e0b970c2f0c2fd9f6457f2
roderickalew <roderickalew@gmail.com>
2021-10-28 07:57:37 -0600

    update

--- CustomerData.sql
+++ CustomerData.sql
@@ -8,6 +8,7 @@ create table shoppingListTable (
     id serial primary key, 
     productName varchar(100) not null, 
     quantity int not null,
+
     clientID int REFERENCES clientTable(id)
 );
 
@@ -15,8 +16,8 @@ create table shoppingExpenseTable (
     id serial primary key, 
     orderID int REFERENCES orderTable(id),
     itemID INT REFERENCES clientTable(id),
-    price real not null, 
-    subtotal real not null
+    price double precision null, 
+    subtotal double precision  null
 );
 
 create table orderTable (
@@ -31,8 +32,28 @@ drop table shoppingExpenseTable;
 drop table orderTable;
 drop table clientTable; 
 
+select * from shoppingExpenseTable; 
+
 select * from clientTable; 
 select * from shoppingListTable; 
+
+insert into clientTable(clientName, clientEmail) values
+('James', 'Jdoe@notreal.com'), ('John', 'Jon@notreal.com');
+
+insert into shoppingListTable(productName, quantity, clientID) values
+('Outback Lager', 2, 1), ('Chartreuse verte', 5, 1), ('Ipoh Coffee', 7, 2),('Chai', 3, 2); 
+
+insert into shoppingListTable(productName, quantity, clientID) values
+('Aniseed Syrup', 2, 1);
+
+insert into shoppingListTable(productName, quantity, clientID) values
+('Comics', 3, 2);
+
+
+
+
+('Jane', 'Jae@notreal.com'),('Chris', 'Chris@notreal.com'), ('Frank', 'frank@notreal.com');
+
 
 
 
@@ -49,7 +70,6 @@ create table teachers(
 
 insert into Students(studentName)
 ('James'), ('Jack'), ('Jane'),('Jas'), ('Jak'), ('Jae'),('Jes'), ('Jck'), ('Ja');
-
 
 insert into teachers(teacherName)
 ('Tim'), ('Tom'), ('Timmy'),('Tommy'), ('TI'), ('TO'),('Tes'), ('Tck'), ('Ta');
